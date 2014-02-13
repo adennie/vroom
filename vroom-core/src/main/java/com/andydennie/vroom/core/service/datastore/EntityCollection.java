4c2fe275cbeb34195db5ba7e46848e905d9e9582
mohit <mohitguptajan85@gmail.com>
2014-02-13 17:42:37 -0500

    Added rewriteAll() function to force migration on all entities

--- vroom-core/src/main/java/com/andydennie/vroom/core/service/datastore/EntityCollection.java
+++ vroom-core/src/main/java/com/andydennie/vroom/core/service/datastore/EntityCollection.java
@@ -83,4 +83,12 @@ public abstract class EntityCollection<
     public E getElementEntity() {
         return mElementEntity;
     }
+
+    //load and save all entities to completely migration to a new version of DAO .
+    protected void rewriteAll() {
+        List<DAO> daos = ofy().load().type(mElementEntity.getDaoClass()).list();
+        for (DAO dao : daos)    {
+            mElementEntity.saveDao(dao);
+        }
+    }
 }
